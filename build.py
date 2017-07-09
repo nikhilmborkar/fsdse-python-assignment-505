befb3ebfee3934ce6479436a455e33fbf3e2b78a
nikhilmborkar <nikhilmukundborkar@gmail.com>
2017-07-09 16:11:18 +0000

    Done

--- build.py
+++ build.py
@@ -1,34 +1,34 @@
 import pandas as pd
-
-
 def load_data():
-    """
-        Enter your code here
-    """
-
+    df = pd.read_csv('files/olympics.csv',skiprows = 1)
+    for column in df.columns:
+        if '01' in column:
+            df.rename(inplace=True, columns = {column:column.replace("01 !","Gold")})
+        elif '02' in column:
+            df.rename(inplace=True, columns = {column:column.replace("02 !","Silver")})
+        elif '03'in column:
+            df.rename(inplace=True, columns = {column:column.replace("03 !","Bronze")})
+        else:
+            pass
+    country_names = [x.split('\xc2\xa0(')[0] for x in df.iloc[:,0]]
+    df.set_index(pd.Series(country_names), inplace=True)
+    df.rename(columns={"Unnamed: 0" : "Country"},inplace = True)
+    df.iloc[:,0] = country_names
+    df.drop(['Totals'],axis=0, inplace= True)
+    return df
 
 def first_country(df):
-    """
-        Enter your code here
-    """
-
+    return df.iloc[0,:]
 
 def gold_medal(df):
-    """
-        Enter your code here
-    """
-
+    return df['Gold'].argmax()
 
 def biggest_difference_in_gold_medal(df):
-    """
-        Enter your code here
-    """
+    return (df['Gold']-df['Gold.1']).argmax()
 
 def get_points(df):
-    """
-    Enter your code here
-    """
-
+    df['Points'] = 3*df.iloc[:,12] + 2*df.iloc[:,13] + df.iloc[:,14]
+    return df.loc[:,'Points']
 
 # df = load_data()
 # print(first_country(df)["# Summer"])
